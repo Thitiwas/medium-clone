c88ed08fd927e08a4091b73739c9177034328e3b
zesani <7sin@outlook.co.th>
2016-10-30 00:24:28 +0700

    edit delete

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="hello">
-    <div class="center-column" v-for="story in state.storys">
-      <router-link :to="{ name: 'story', params: { id:story.id }}">
-        <h3 class="title is-3 title-head">{{ story.title }}</h3>
-        <p class="subtitle is-5 body-content">{{ story.body.substr(0, 200) }} <br>Read more. . .</p>
-      </router-link>
-      <div class="control is-grouped  button-delete">
-       <p class="control">
-        <a class="button is-danger" @click="deleteStory(story.id)">
-          Delete story
-        </a>
-      </p>
+    <div class="" v-for="story in state.storys">
+      <div class="center-column" >
+        <router-link :to="{ name: 'story', params: { id:story.id }}">
+          <h3 class="title is-3 title-head">{{ story.title }}</h3>
+          <p class="subtitle is-5 body-content">{{ story.body.substr(0, 200) }} <br>Read more. . .</p>
+        </router-link>
       </div>
+      <button class="button-delete" name="button" @click="deleteStory(story.id)">  Delete story</button>
     </div>
+
   </div>
 </template>
 
@@ -29,14 +26,14 @@ export default {
 .center-column {
   font-family: 'Raleway', sans-serif;
   margin-top: 20px;
-  margin-bottom: 10px;
+  margin-bottom: 5px;
   background-color: #ffffff;
   width: 50%;
   margin-left: 25%;
   margin-right: 25%;
   border: 1px solid #d1d1d1;
   padding-left: 20px;
-  /*padding-right: 20px;*/
+  padding-right: 20px;
   padding-top: 20px;
   padding-bottom: 15px;
 }
@@ -48,8 +45,16 @@ export default {
   line-height: 30px;
 }
 .button-delete {
-  width: 15%;
-  margin-left: 85%;
-  margin-right: 0%;
+  font-family: 'Raleway', sans-serif;
+  cursor: pointer;
+  font-size: 1.5em;
+  color: white;
+  border-radius: 10px;
+  border: 0px solid;
+  background-color: #ff1f4b;
+  height: 40px;
+  /*width: 15%;*/
+  margin-left: 65%;
+  margin-right: 25%;
 }
 </style>
